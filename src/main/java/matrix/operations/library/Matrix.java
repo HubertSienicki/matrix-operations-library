f5d48718381714d3473c908214ca625cd0c60994
HubertSienicki <businesssienicki@gmail.com>
2021-12-26 15:12:09 +0000

    added two helper methods for matrix inversion

--- src/main/java/matrix/operations/library/Matrix.java
+++ src/main/java/matrix/operations/library/Matrix.java
@@ -10,14 +10,15 @@ package matrix.operations.library;
  * @author kneiv
  */
 public class Matrix {
+
     private int[][] matrix;
 
     public Matrix(int[][] matrix) {
         this.matrix = matrix;
     }
-    
-    public Matrix(){
-        
+
+    public Matrix() {
+
     }
 
     public int[][] getMatrix() {
@@ -27,6 +28,7 @@ public class Matrix {
     public void setMatrix(int[][] matrix) {
         this.matrix = matrix;
     }
+
     /*TODO:
         1.Matrix printing : DONE
         2.Matrix multiplication : DONE
@@ -42,29 +44,28 @@ public class Matrix {
         12.Adding a number to an index
         13.Multiply all
         14.SORT matrices
-    */
-    
+     */
+
     //Function for printing out loaded matrix
-    public void print(){
-        for(int i = 0; i < this.matrix.length; i++) {
+    public void print() {
+        for (int i = 0; i < this.matrix.length; i++) {
             for (int j = 0; j < this.matrix[i].length; j++) {
                 System.out.print(this.matrix[i][j] + ", ");
             }
             System.out.println("");
         }
     }
-    
-     public void print(int[][] matrix){
-        for(int i = 0; i < matrix.length; i++) {
+
+    public void print(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
                 System.out.print(matrix[i][j] + ", ");
             }
             System.out.println("");
         }
     }
-    
-    
-    public int[][] multiplyMatrices(int[][] firstMatrix, int[][] secondMatrix){
+
+    public int[][] multiplyMatrices(int[][] firstMatrix, int[][] secondMatrix) {
         int[][] result = new int[firstMatrix.length][secondMatrix[0].length];
         for (int i = 0; i < result.length; i++) {
             for (int j = 0; j < result[i].length; j++) {
@@ -73,19 +74,19 @@ public class Matrix {
         }
         return result;
     }
-    
-    public int[][] multiplyMatrices(int[][] secondMatrix){
+
+    public int[][] multiplyMatrices(int[][] secondMatrix) {
         int[][] result = new int[this.matrix.length][secondMatrix[0].length];
-        
-        for (int i = 0; i < result.length ; i++) {
+
+        for (int i = 0; i < result.length; i++) {
             for (int j = 0; j < result[i].length; j++) {
                 result[i][j] = multiplyCells(this.matrix, secondMatrix, i, j);
             }
         }
         return result;
     }
-    
-    public int[][] addMatrices(int[][] secondMatrix){
+
+    public int[][] addMatrices(int[][] secondMatrix) {
         int[][] result = new int[this.matrix.length][secondMatrix[0].length];
         for (int i = 0; i < result.length; i++) {
             for (int j = 0; j < result[i].length; j++) {
@@ -94,8 +95,8 @@ public class Matrix {
         }
         return result;
     }
-    
-    public int[][] addMatrices(int[][] firstMatrix, int[][] secondMatrix){
+
+    public int[][] addMatrices(int[][] firstMatrix, int[][] secondMatrix) {
         int[][] result = new int[firstMatrix.length][secondMatrix[0].length];
         for (int i = 0; i < result.length; i++) {
             for (int j = 0; j < result[i].length; j++) {
@@ -104,11 +105,11 @@ public class Matrix {
         }
         return result;
     }
-    
+
     //subtracts a variable matrix from a loaded matrix
-    public int[][] subtract(int[][] secondMatrix){
+    public int[][] subtract(int[][] secondMatrix) {
         int[][] result = new int[this.matrix.length][secondMatrix[0].length];
-        
+
         for (int i = 0; i < result.length; i++) {
             for (int j = 0; j < result[i].length; j++) {
                 result[i][j] = this.matrix[i][j] - secondMatrix[i][j];
@@ -116,11 +117,11 @@ public class Matrix {
         }
         return result;
     }
-    
+
     //subtracts an two matrices from eachother
-    public int[][] subtract(int[][] firstMatrix, int[][] secondMatrix){
+    public int[][] subtract(int[][] firstMatrix, int[][] secondMatrix) {
         int[][] result = new int[firstMatrix.length][secondMatrix[0].length];
-        
+
         for (int i = 0; i < result.length; i++) {
             for (int j = 0; j < result[i].length; j++) {
                 result[i][j] = firstMatrix[i][j] - secondMatrix[i][j];
@@ -128,29 +129,57 @@ public class Matrix {
         }
         return result;
     }
-    
-    
-    
-    
-    
-    
-    
-    
+
     //---------Helper Methods-----------//
-    
     //Used in matrix multiplication to multiply row of cells
-    private int multiplyCells(int[][] firstMatrix, int[][] secondMatrix, int row, int col){
+    private int multiplyCells(int[][] firstMatrix, int[][] secondMatrix, int row, int col) {
         int cell = 0;
         for (int i = 0; i < secondMatrix.length; i++) {
             cell += firstMatrix[row][i] * secondMatrix[i][col];
         }
         return cell;
     }
-    
+
     //returns dimension of a matrix
-    private int calculateDimensions(int[][] matrix){
+    private int calculateDimensions(int[][] matrix) {
         int result;
         result = matrix.length * matrix[0].length;
         return result;
+    }
+    
+    private int findOrder(int[][] matrix){
+        return matrix.length
+    }
+    
+    //only works for square matrices (N is the order of the square matrix)
+    private int findDeterminant(int[][] matrix, int N) {
+        int det;
+        switch (N) {
+            case 1 ->
+                det = matrix[0][0];
+            case 2 ->
+                det = matrix[0][0] * matrix[1][1] - matrix[1][0] * matrix[0][1];
+            default -> {
+                det = 0;
+                for (int j1 = 0; j1 < N; j1++) {
+                    int[][] m = new int[N - 1][];
+                    for (int k = 0; k < (N - 1); k++) {
+                        m[k] = new int[N - 1];
+                    }
+                    for (int i = 1; i < N; i++) {
+                        int j2 = 0;
+                        for (int j = 0; j < N; j++) {
+                            if (j == j1) {
+                                continue;
+                            }
+                            m[i - 1][j2] = matrix[i][j];
+                            j2++;
+                        }
+                    }
+                    det += Math.pow(-1.0, 1.0 + j1 + 1.0) * matrix[0][j1] * findDeterminant(matrix, N-1);
+                }
+            }
+        }
+        return det;
     }
 }
